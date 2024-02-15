4e15a64418637624e68622a3d56609f199edf815
diyorbeksafarov2002 <diyorbeksafarov4@gmail.com>
2024-02-15 19:12:39 +0300

    first commit

--- nikatin/settings.py
+++ nikatin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-m_js5bd#4@0@o7y-=ls7#cxf-vdm)zuhbw245(2yrybn_19*#7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
